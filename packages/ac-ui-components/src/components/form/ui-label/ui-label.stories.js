be5451708195d5b104bde979d3110a3a4b2d54cd
Ozeas Santos <computer.ozeas@gmail.com>
2020-08-16 03:46:41 -0300

    feat: add others tests to ui-label

--- packages/ac-ui-components/src/components/form/ui-label/ui-label.stories.js
+++ packages/ac-ui-components/src/components/form/ui-label/ui-label.stories.js
@@ -1,10 +1,15 @@
 import { storiesOf } from '@storybook/web-components';
+import '@webcomponents/webcomponentsjs/webcomponents-bundle';
 
 import './ui-label';
 
-storiesOf('UILabel', module).add('default view', () => {
-  const el = document.createElement('ui-label');
-  el.for = 'test';
-  el.innerHTML = 'test';
-  return el;
+storiesOf('UILabel', module).add('with basic usage', () => '<ui-label>Simple label</ui-label>');
+
+storiesOf('UILabel', module).add('with input referenced', () => {
+  const wrapper = `
+    <ui-label for="entry-example">Custom label</ui-label>
+    <input id="entry-example" type="text" />
+  `;
+
+  return wrapper;
 });
