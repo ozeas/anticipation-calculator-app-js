3b4fee8e7e4e31d3fbba3e50f37d34efade442ac
Ozeas Santos <computer.ozeas@gmail.com>
2020-08-15 21:11:24 -0300

    feat: add storybook to doc components

--- packages/ac-ui-components/src/components/form/ui-label/ui-label.js
+++ packages/ac-ui-components/src/components/form/ui-label/ui-label.js
@@ -5,7 +5,7 @@ import '@webcomponents/webcomponentsjs/webcomponents-bundle';
   template.innerHTML = `
     <style>
       :host {
-        cursor: default;
+        cursor: pointer;
       }
     </style>
     <slot></slot>
@@ -16,6 +16,17 @@ import '@webcomponents/webcomponentsjs/webcomponents-bundle';
   class UILabel extends HTMLElement {
     static get observedAttributes() {
       return ['for'];
+    }
+
+    constructor() {
+      super();
+
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+      console.log(this.for);
     }
   }
 
